d1197020b86bdce81d684114f6847cebe75b46fb
daili <daili@dhcp-100-99-200-127.mtv.corp.google.com>
2018-05-17 19:13:04 -0700

    Initial checkin

---
+++ GCodeRetreat/src/experimental/users/srulix/references/tests/Java.java
@@ -0,0 +1,9 @@
+package experimental.users.srulix.references.tests;
+
+public class Java {
+  public Java() {}
+
+  public String bar() {
+    return "fubar";
+  }
+}
---
+++ GCodeRetreat/src/experimental/users/srulix/references/tests/JavaTest.java
@@ -0,0 +1,17 @@
+package experimental.users.srulix.references.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class JavaTest {
+
+  @Test
+  public void fooBar() {
+    Java j = new Java();
+    assertEquals("fubar", j.bar());
+  }
+}
